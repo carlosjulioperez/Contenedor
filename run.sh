5220b25506b0094e331c874a9e3617e21c3af7b4
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2020-09-10 00:52:53 -0400

    Ajustes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-DIR=~/local/project/workspace
+DIR=~/local/openxava-6.3.2/workspace
 DIR1=$DIR/Contenedor/web/WEB-INF
 DIR2=$DIR/OpenXava/lib
 java -cp $DIR1/classes:$DIR1/lib/*:$DIR2/* _run._Run_Contenedor
